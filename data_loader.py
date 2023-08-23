9372a930685c99a5d3e6d454d22f5c76f46e88bd
mbruhns <matthias.bruhns@uni-tuebingen.de>
2023-08-23 17:10:53 +0200

    Cleanup.

---
+++ data_loader.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import tifffile
+
+
+class TileLoader:
+    def __init__(self, directory):
+        self.directory = Path(directory)
+
+
+def main():
+    print("Main")
+
+
+if __name__ == "__main__":
+    main()
