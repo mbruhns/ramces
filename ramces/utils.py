3c8a885219e3906f4bd3de741537aef6043bd681
mbruhns <matthias.bruhns@uni-tuebingen.de>
2023-08-24 14:52:03 +0200

    Initial commit.

---
+++ ramces/utils.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from tifffile import tifffile
+
+
+class DataLoader:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def generate_tiles(self):
+        pass
